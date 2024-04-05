6b023dd78717cf29f9d789165961550fd1ca0a0d
annv <annv@coccoc.com>
2024-04-05 16:06:25 +0700

    added an example for mutex1

--- apue.3e/threads/mutex1.c
+++ apue.3e/threads/mutex1.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <pthread.h>
 
 struct foo {
@@ -30,6 +31,7 @@ foo_hold(struct foo *fp) /* add a reference to the object */
 {
 	pthread_mutex_lock(&fp->f_lock);
 	fp->f_count++;
+	printf("Thread holding foo with ID %d, count: %d\n", fp->f_id, fp->f_count);
 	pthread_mutex_unlock(&fp->f_lock);
 }
 
@@ -38,10 +40,48 @@ foo_rele(struct foo *fp) /* release a reference to the object */
 {
 	pthread_mutex_lock(&fp->f_lock);
 	if (--fp->f_count == 0) { /* last reference */
+	    printf("Releasing foo with ID %d\n", fp->f_id);
 		pthread_mutex_unlock(&fp->f_lock);
 		pthread_mutex_destroy(&fp->f_lock);
 		free(fp);
 	} else {
+        printf("Thread releasing reference to foo with ID %d, count: %d\n", fp->f_id, fp->f_count);
 		pthread_mutex_unlock(&fp->f_lock);
 	}
+}
+
+/* Thread function to manipulate foo */
+void *thread_func(void *arg) {
+    struct foo *myFoo = (struct foo *)arg;
+
+    /* Hold the foo object (add a reference) */
+    foo_hold(myFoo);
+
+    /* Simulate some work */
+    usleep(100000); /* Sleep for 100 milliseconds */
+
+    /* Release the foo object (release a reference) */
+    foo_rele(myFoo);
+
+    return NULL;
+}
+
+int main() {
+
+    /* Allocate a new foo object */
+    struct foo *myFoo = foo_alloc(123);
+
+    /* Create threads to manipulate the foo object */
+    pthread_t tid[5];
+    int i;
+	for (i = 0; i < 5; i++) {
+        pthread_create(&tid[i], NULL, thread_func, myFoo);
+    }
+
+    /* Join threads */
+    for (i = 0; i < 5; i++) {
+        pthread_join(tid[i], NULL);
+    }
+
+    return 0;
 }
